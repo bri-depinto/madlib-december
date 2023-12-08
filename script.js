9afb99efa52a97436af7119f09870ce105df9859
bri-depinto <briannadepinto@gmail.com>
2023-12-08 09:29:12 -0500

    added jaVa

---
+++ script.js
@@ -0,0 +1,10 @@
+let adjective = prompt('enter an adjective:')
+let noun = prompt('enter an noun:')
+let verb = prompt('enter an verb:')
+let adverb = prompt('enter an adverb:')
+
+let story = `Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb} `
+
+let madLibOutputDiv = document.getElementById('madLibOutput')
+
+madLibOutputDiv.innerHTML = `<p> ${story} </p>`
