385d905ee3345956fbfd892ca8772a899db3f501
bri-depinto <144465570+bri-depinto@users.noreply.github.com>
2023-12-20 19:41:29 +0000

    this isnt working

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let noun = prompt('enter an noun:')
 let verb = prompt('enter an verb:')
 let adverb = prompt('enter an adverb:')
 
-let story = `Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb} `
+let story = `Once upon a time in a ${adjective} wonderland, I ${adverb} wrapped gifts for my family. I wrapped them in ${pluralnoun} `
 
 let madLibOutputDiv = document.getElementById('madLibOutput')
 
